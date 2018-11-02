816b85228626275b2efff4730b913d0d8c209ef6
Michael Nutt <michaeltnutt@gmail.com>
2018-11-02 11:44:47 -0500

    Updated database class with needed functions

--- includes/Database.php
+++ includes/Database.php
@@ -11,23 +11,55 @@ class Database
 
   // Connect to Database
   public function conn(){
+    // Connects to the server
     $conn = new mysqli($server, $username, $password, $dbname);
 
+    // Checks if connection was Successful
     if($conn->connect_error){
-      echo "<script>console.log('Connection Failed')</script>";
+      echo "<script>console.log("Connection Failed")</script>";
       die("Connection Failed" . $conn->connect_error);
     }
-    echo "<script>console.log('Connection Successful')</script>";
+    // Prints if was successful
+    echo "<script>console.log("Connection Successful")</script>";
     $conn->query("USE hotel");
     return $conn;
   }
 
+  // Select statement
   public function select($what, $where){
+    // Connect to database
+    $conn = $this->conn();
+
+    // Selects specified data from specified table
+    $sql = "SELECT " . $what . " FROM " . $where;
+    $result = $conn->query($sql);
+
+    $conn->close();
+
     return $results;
   }
 
-  public function insert($what, $where){
-    return;
+  // Selects specific data using where statement
+  public function select_specific($what, $where, $which){
+    $conn = $this->conn();
+
+    // Selects specified data from matching specified paramaters
+    $sql = "SELECT " . $what . " FROM " . $where . " WHERE " . $which;
+    $result = $conn->query($sql);
+
+    $conn->close();
+
+    return $result;
+  }
+
+  public function insert($whatFields, $whatData, $where){
+    $conn = $this->conn();
+
+    $sql = "INSERT INTO " . $where . "(" . $whatFields . ") VALUES (" . $whatData . ")";
+    $result = $conn->query($sql);
+
+    $conn->close();
+    return $result;
   }
 }
 ?>
