0c5f6947ee65e8df73e24cb4b861a3186e256316
Michael Nutt <michaeltnutt@gmail.com>
2018-11-02 16:06:34 +0000

    Added index.php test to the git

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Hotel Management System</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Hotel Management System In Progress!</h1>
+</body>
+</html>
