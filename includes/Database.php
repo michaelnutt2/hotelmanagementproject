c53647d141f6a49af076a7b7eadbd89676ae9328
Michael Nutt <michaeltnutt@gmail.com>
2018-11-02 12:30:43 -0500

    Updated database class with correct calls

--- includes/Database.php
+++ includes/Database.php
@@ -12,7 +12,7 @@ class Database
   // Connect to Database
   public function conn(){
     // Connects to the server
-    $conn = new mysqli($server, $username, $password, $dbname);
+    $conn = new mysqli($this->server, $this->username, $this->password, $this->dbname);
 
     // Checks if connection was Successful
     if($conn->connect_error){
@@ -32,34 +32,34 @@ class Database
 
     // Selects specified data from specified table
     $sql = "SELECT " . $what . " FROM " . $where;
-    $result = $conn->query($sql);
+    $this->results = $conn->query($sql);
 
     $conn->close();
 
-    return $results;
+    return $this->results;
   }
 
   // Selects specific data using where statement
-  public function select_specific($what, $where, $which){
+  public function select_one($what, $where, $which){
     $conn = $this->conn();
 
     // Selects specified data from matching specified paramaters
     $sql = "SELECT " . $what . " FROM " . $where . " WHERE " . $which;
-    $result = $conn->query($sql);
+    $this->results= $conn->query($sql);
 
     $conn->close();
 
-    return $result;
+    return $this->results;
   }
 
   public function insert($whatFields, $whatData, $where){
     $conn = $this->conn();
 
     $sql = "INSERT INTO " . $where . "(" . $whatFields . ") VALUES (" . $whatData . ")";
-    $result = $conn->query($sql);
+    $this->results = $conn->query($sql);
 
     $conn->close();
-    return $result;
+    return $this->results;
   }
 }
 ?>
