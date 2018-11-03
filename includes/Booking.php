68ae4ab771028a870c4c0c722c21e798ab55415d
Michael Nutt <michaeltnutt@gmail.com>
2018-11-03 04:14:52 -0500

    Updated Booking class with function definitions

--- includes/Booking.php
+++ includes/Booking.php
@@ -1,21 +1,34 @@
 <?php
+include("Database.php");
 //This is the bookings class
-class Bookings
+class Booking
 {
-    //Properties
-    private $results = [];
+  // Variables
+  private $db;
 
-    //Methods
-    public function select_all(){
-        return 0;
-    }
+  function __construct(){
+    $this->db = new Database;
+  }
 
-    public function select_one(){
-        return 0;
-    }
+  //Methods
+  // Selects all from booking table
+  public function select_all(){
+    $results = $this->db->select("*", "booking");
+    return $results;
+  }
 
-    public function create_new(){
-        return 0;
-    }
+  // selects specified row, based on type and value for the search.
+  public function select_one($type, $value){
+    $query = $type."=".$value;
+    $results = $this->db->select_one("*","booking", $query);
+    return $results;
+  }
+
+  // Inserts new booking into database
+  public function create_new($guestID, $roomID, $start, $stop){
+    $query = $roomID.",".$guestID.",".$start.",".$stop;
+    $this->db->insert("Guest_ID, Room_ID, start, stop", $query, "booking");
+    return ;
+  }
 }
 ?>
