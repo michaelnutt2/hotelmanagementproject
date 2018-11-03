e3ba4e016ee0917a47f2db4fe1dc642bccea9961
Michael Nutt <michaeltnutt@gmail.com>
2018-11-03 02:22:45 -0500

    Created test file for debugging PHP classes

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+<html>
+<head>
+  <title>Test Page</title>
+  <?php include($_SERVER['DOCUMENT_ROOT']."/includes/*");?>
+</head>
+<body>
