b17e12bc49aba2442cceddd4672a82dc2451e403
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-05 12:31:16 -0600

    centered images

--- home.php
+++ home.php
@@ -7,7 +7,8 @@
   <?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");?>
 <div class="image">
 <center><h1 class="text-info"> Hello, "NAME"! </h1>
-  <br> <h3>Welcome to Heartbreak Hotel!</h3></center>
+  <br> <h3>Welcome to Heartbreak Hotel!</h3>
   <img src="images/HH3.jpg" alt="Heartbreak Hotel" width="755" height="600" alt=""/>
   <img src="images/HH2.jpg" alt="Heartbreak Hotel" width="755" height="600" alt=""/>
+</center>
 </div>
