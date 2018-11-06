947a468c4b9e70f75bd3e500796569bb1a39f320
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-06 14:37:17 -0600

    Updated booking page && added the confirmbooking page

--- booking.php
+++ booking.php
@@ -5,6 +5,30 @@
 </head>
 <body>
   <?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");?>
-  <h1>Booking</h1>
-</body>
-</html>
+
+  <!--- INSERT FORM TO BOOK HERE  --->
+  <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
+    <fieldset>
+      <legend>Search Rooms</legend>
+      <div class="form-group">
+        <label for="numOfGuests">Number of Beds</label>
+        <input type="number" class="form-control" id="numOfGuests" placeholder="Enter Number of Beds">
+      </div>
+      <div class="form-group">
+        <label for="startDate">Check-In Date</label>
+        <input type="date" class="form-control" id="startDate" placeholder="00/00/0000">
+      </div>
+      <div class="form-group">
+        <label for="endDate">Check-Out Date</label>
+        <input type="date" class="form-control" id="endDate" placeholder="00/00/0000">
+      </div>
+  <!--- Print out the available rooms here???? --->
+  <!--- Michael, php will need to be placed here to populate the available rooms--->
+    <button type="submit" class="btn btn-primary">Find Rooms</button>
+    <br>
+  </fieldset>
+  </form>
+  <!--- END FORM TO BOOK HERE  --->
+
+  </body>
+  </html>
---
+++ confirmBooking.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <title>Booking</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");?>
+
+<div class="jumbotron">
+  <center>
+     <h1 class="display-3">Room Booked Successfully!</h1>
+  <p class="lead">Confirmation Number: "NUMBER"</p>
+    <p class="lead">Room Number: "ROOM NUMBER"</p>
+    <p class="lead">Check-In Date: "DATE"</p>
+    <p class="lead">Check-Out Date: "DATE"</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="home.php" role="button">Go Home</a>
+  </p>
+  </center>
+</div>
+
+</body>
+</html>
