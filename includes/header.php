23f10587dfce25a028e42e67a40909dc110cecc7
Michael Nutt <michaeltnutt@gmail.com>
2018-11-10 04:11:40 +0000

    Update take 4

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
   session_start();
   if(!isset($_SESSIN['name']))
   {
-  	header('Location: "$_SERVER['DOCUMENT_ROOT']"./hotelmanagement/index.php');
+  	header('Location: /var/www/html/hotelmanagement/index.php');
 	exit();
   }
   error_reporting(E_ALL);
