429e72177154458c3fae0ed7a55f6f38402a1582
Cole Bush <cbush311@gmail.com>
2018-11-13 15:25:35 -0600

    Check in and check out pages complete

--- checkout.php
+++ checkout.php
@@ -6,5 +6,44 @@
 <body>
   <?php include($_SERVER['DOCUMENT_ROOT']."/hotelmanagement/includes/header.php");?>
   <h1>Check Out</h1>
+  <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
+  <fieldset>
+    <legend>Welcome to Guest Check Out!</legend>
+    <div class="form-group">
+      <label for="custName">Guest Name</label>
+      <input type="text"  class="form-control" id="custName"  placeholder="Enter First and Last Name">
+    </div>
+    <legend style= "float: left; margin-left: 125px;"> --------- Or ---------</legend>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Guest Email Address</label>
+      <input type="email"  class="form-control" id="exampleInputEmail1"  placeholder="Enter Email">
+    </div>
+    <legend style= "float: left; margin-left: 125px;"> --------- Or ---------</legend>
+    <div class="form-group">
+      <label for="phoneNumber">Guest Room Number</label>
+      <input type="tel" class="form-control" id="phoneNumber" placeholder="Enter Room Number">
+    </div>
+
+    <fieldset class="form-group">
+      <legend>Check Out Check List</legend>
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="" checked="">
+          Two room key cards
+        </label>
+      </div>
+      <div class="form-check disabled">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="" checked="">
+          [2nd Check out option]
+        </label>
+      </div>
+    </fieldset>
+<br>
+<button type="submit" class="btn btn-primary">Check Out</button>
+<button type="submit" class="btn btn-primary">Cancel</button>
+
+  </fieldset>
+</form>
 </body>
 </html>
