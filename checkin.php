d72fe27fdc91b1bb514bebd0ade3c3fbbe04842a
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-16 12:22:21 -0600

    Made changes to the OR and the button

--- checkin.php
+++ checkin.php
@@ -9,8 +9,37 @@
   <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
   <fieldset>
     <legend>Welcome to Guest Check In!</legend>
-   
 
+
+      <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
+      <fieldset>
+        <legend>Welcome to Guest Check In!</legend>
+
+        <div class="form-group">
+          <label for="custName">Guest Name</label>
+          <input type="text"  class="form-control" id="custName"  placeholder="Enter Name">
+        </div>
+    <center><h3> OR </h3></center>
+        <div class="form-group">
+          <label for="phoneNumber">Guest Room Number</label>
+          <input type="tel" class="form-control" id="phoneNumber" placeholder="Enter Room Number">
+        </div>
+
+    <br>
+    <button type="submit" class="btn btn-primary">Search</button>
+
+
+      </fieldset>
+    </form>
+
+    </body>
+    </html>
+
+
+
+
+
+<!---
     <div class="form-group">
       <label for="custName">Guest Name</label>
       <input type="text"  class="form-control" id="custName"  placeholder="Enter Name">
@@ -34,3 +63,4 @@
 
 </body>
 </html>
+--->
