7e31a42c5b9c96608c6e49a880ecf9fa34948fdd
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-16 12:31:52 -0600

    Swapped Check-in and Checkout to satisfy the design

--- checkout.php
+++ checkout.php
@@ -6,6 +6,37 @@
 <body>
   <?php include($_SERVER['DOCUMENT_ROOT']."/hotelmanagement/includes/header.php");?>
   <h1>Check Out</h1>
+
+    <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
+    <fieldset>
+      <legend>Welcome to Guest Check In!</legend>
+
+      <div class="form-group">
+        <label for="custName">Guest Name</label>
+        <input type="text"  class="form-control" id="custName"  placeholder="Enter Name">
+      </div>
+  <center><h3> OR </h3></center>
+      <div class="form-group">
+        <label for="phoneNumber">Guest Room Number</label>
+        <input type="tel" class="form-control" id="phoneNumber" placeholder="Enter Room Number">
+      </div>
+
+  <br>
+  <button type="submit" class="btn btn-primary">Search</button>
+
+
+    </fieldset>
+  </form>
+
+  </body>
+  </html>
+
+
+
+
+
+
+<!---
   <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
   <fieldset>
     <legend>Welcome to Guest Check Out!</legend>
@@ -47,3 +78,4 @@
 </form>
 </body>
 </html>
+-->
