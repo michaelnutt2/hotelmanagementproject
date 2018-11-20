4f9cd21de275f95587df6bf9f4ce263cc28f6cd1
Cole Bush <cbush311@gmail.com>
2018-11-20 15:22:47 -0600

    Documents Page complete with link to pdfs

--- documents.php
+++ documents.php
@@ -7,6 +7,19 @@
   </head>
   <body>
     <?php include($_SERVER['DOCUMENT_ROOT']."/hotelmanagement/includes/header.php");?>
-    <h1>Documents</h1>
+    <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
+  <fieldset>
+    <legend>Click Below to View Employment Documents</legend>
+
+   <a href="Documents/eeopost.pdf" class="btn btn-primary btn-lg">Equal Employment Opportunities</a>
+   <br><br/>
+   <a href="Documents/minwage.pdf" class="btn btn-primary btn-lg">Minimum Wage Law</a>
+   <br><br/>
+   <a href="Documents/osha3165.pdf" class="btn btn-primary btn-lg">Occupational Safety and Health</a>
+
+
+
+  </fieldset>
+</form>
   </body>
 </html>
