5912c5c616b9dffdd9afcc9667b5284891a1d29f
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-24 00:35:54 -0600

    checkout confirm made

---
+++ checkoutConfirm.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+  <title>Booking</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php include($_SERVER['DOCUMENT_ROOT']."/hotelmanagement/includes/header.php");?>
+  <!--- INSERT FORM TO BOOK HERE  --->
+
+
+<div class="jumbotron">
+  <center>
+     <h1 class="display-5">Guest Checked Out </h1>
+    <p class="lead">Room Number: "ROOM NUMBER"</p>
+    <p class="lead">Guest Name: "NAME"</p>
+    <p class="lead">Guest Email: "Email"</p>
+<hr>
+
+    <p class="lead">Duration of Stay: "stay"</p>
+    <p class="lead">Room Rate: "rate"</p>
+    <p class="lead">Sales Tax: "tax"</p>
+<hr>
+    <p class="lead">Total: "total"</p>
+<hr>
+  <p class="lead">Charges have been applied to the card on file. </p>
+
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="home.php" role="button">Go Home</a>
+  </p>
+  </center>
+</div>
+
+
+
+</body>
+</html>
