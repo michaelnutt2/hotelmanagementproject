575030d53f2e9e28729e19d0e84aae0b09e8b381
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-25 19:23:42 -0600

    Finished Time off Request Pages

--- TimeOffRequestE.php
+++ TimeOffRequestE.php
@@ -22,12 +22,47 @@
 
 <!---End second navbar --->
 
+<!--- INSERT FORM TO Request off HERE  --->
+<form style= "margin-left:500px; margin-right:500px; margin-top:20px">
+  <fieldset>
+    <legend>Time Off Request</legend>
 
+    <div class="form-group">
+  <select class="custom-select">
+    <option selected="">Reason for Request Off</option>
+    <option value="1">Vacation</option>
+    <option value="2">Sick</option>
+    <option value="3">Maternity/Paternity</option>
+    <option value="4">Emergency</option>
+    <option value="5">Bereavement</option>
+    <option value="6">Other</option>
+  </select>
+</div>
 
+    <div class="form-group">
+      <label for="startDate">Start Date</label>
+      <input type="date" class="form-control" id="startDate" placeholder="00/00/0000">
+    </div>
+    <div class="form-group">
+      <label for="endDate">End Date</label>
+      <input type="date" class="form-control" id="endDate" placeholder="00/00/0000">
+    </div>
+    <div class="form-group">
+      <label for="startDate">Start Time</label>
+      <input type="time" class="form-control" id="startTime" placeholder="HH:MM">
+    </div>
+    <div class="form-group">
+      <label for="endDate">End Time</label>
+      <input type="time" class="form-control" id="endTime" placeholder="HH:MM">
+    </div>
 
-<h1>Time off Request </h1>
-
-
+  <button type="submit" class="btn btn-primary">Submit Request</button>
+  <br>
+</fieldset>
+</form>
+<!--- END FORM TO Request off HERE  --->
+<br><br>
+<h3 style="display: none;"><center>Time Off Request Submitted </center></h3>
 
 </body>
 </html>
