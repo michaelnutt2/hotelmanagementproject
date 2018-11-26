f3ec7c734e57990fa952ddb6909f5beaf528a1d8
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-25 18:30:56 -0600

    Created Pages, not complete.

---
+++ SchedulePendingMan.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Hotel Management System</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!---NavBar Start --->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Schedules</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Documents</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Check-In</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Check-Out</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Booking</a>
+      </li>
+  </ul>
+  </div>
+</nav>
+<!---NavBar End --->
+
+
+<!---Start second navbar --->
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link" data-toggle="tab" href="SchedViewMan.html">View All Schedules</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active show" data-toggle="tab" href="ScheduleSetMan.html">Set Schedules</a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link active show" data-toggle="tab" href="ScheduleslePendingMan.html">Pending Time Off Requests</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active show" data-toggle="tab" href="TimeOffRequest.html">Time Off Request</a>
+  </li>
+</ul>
+
+<!---End second navbar --->
+
+
+
+<h1>Schedule Pending time off requests</h1>
+
+
+
+</body>
+</html>
