0e5448e48142973d6f4409a4b66dde5dc84a0f37
Michael Nutt <michaeltnutt@gmail.com>
2018-11-27 02:33:05 -0600

    Set up functionality for checkout pages

--- checkoutConfirm.php
+++ checkoutConfirm.php
@@ -6,21 +6,32 @@
 <body>
   <?php include($_SERVER['DOCUMENT_ROOT']."/hotelmanagement/includes/header.php");?>
   <!--- INSERT FORM TO BOOK HERE  --->
-
+  <?php
+    $brow = $_SESSION["bResult"];
+    $grow = $_SESSION["gResult"];
+  ?>
 
 <div class="jumbotron">
   <center>
      <h1 class="display-5">Guest Checked Out </h1>
-    <p class="lead">Room Number: "ROOM NUMBER"</p>
-    <p class="lead">Guest Name: "NAME"</p>
-    <p class="lead">Guest Email: "Email"</p>
+    <p class="lead">Room Number: <?php echo $brow["Room_ID"];?></p>
+    <p class="lead">Guest Name: <?php echo $grow["Name"];?></p>
+    <p class="lead">Guest Email: <?php echo $grow["Email"];?></p>
 <hr>
+  <?php
+    $start = strtotime($brow["start"]);
+    $stop = strtotime($brow["stop"]);
+    $duration = date('Ymd',$stop) - date('Ymd',$start);
+    $cost = 100 * $duration;
+    $tax = $cost * 0.0825;
+    $total = $cost + $tax;
+  ?>
 
-    <p class="lead">Duration of Stay: "stay"</p>
-    <p class="lead">Room Rate: "rate"</p>
-    <p class="lead">Sales Tax: "tax"</p>
+    <p class="lead">Duration of Stay: <?php echo $duration." days";?></p>
+    <p class="lead">Room Rate: $<?php echo $cost; ?></p>
+    <p class="lead">Sales Tax: $<?php echo $tax; ?></p>
 <hr>
-    <p class="lead">Total: "total"</p>
+    <p class="lead">Total: $<?php echo $total; ?></p>
 <hr>
   <p class="lead">Charges have been applied to the card on file. </p>
 
