b0ba722952a817a607a986abad71557760c86a7c
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-30 10:09:24 -0600

    Added Images to the documents page

--- documents.php
+++ documents.php
@@ -7,19 +7,28 @@
   </head>
   <body>
     <?php include("includes/header.php");?>
-    <form style= "margin-left:500px; margin-right:500px; margin-top:20px">
-  <fieldset>
-    <legend>Click Below to View Employment Documents</legend>
 
-   <a href="Documents/eeopost.pdf" class="btn btn-primary btn-lg">Equal Employment Opportunities</a>
-   <br><br/>
-   <a href="Documents/minwage.pdf" class="btn btn-primary btn-lg">Minimum Wage Law</a>
-   <br><br/>
-   <a href="Documents/osha3165.pdf" class="btn btn-primary btn-lg">Occupational Safety and Health</a>
+    <center>
+    <form>
+    <a href="Documents0/eeopost.pdf">
+    <img src="Documents0/EEo.jpg" alt="EEO" style="width:300px;height:400px;">
+    </a>
+
+    <a href="Documents0/osha3165.pdf">
+    <img src="Documents0/osha.jpg" alt="OSHA" style="width:300px;height:400px;">
+    </a>
+
+    <a href="Documents0/minwage.pdf">
+    <img src="Documents0/minwage.jpg" alt="MinWage" style="width:300px;height:400px;">
+    </a>
+
+    <a href="Documents0/fmla.pdf">
+    <img src="Documents0/fmla.jpg" alt="FMLA" style="width:300px;height:400px;">
+    </a>
+
+    <form>
+    </center>
 
 
-
-  </fieldset>
-</form>
   </body>
 </html>
