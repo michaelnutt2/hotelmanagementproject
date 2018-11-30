c000cc16a88df248a41e112ee16ef4f5ed52536c
Michael Nutt <michaeltnutt@gmail.com>
2018-11-29 23:43:41 -0600

    Testing includes options

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 </head>
 <body>
   <?php
-    include($_SERVER['DOCUMENT_ROOT']."/hotelmanagement/includes/header.php");?>
+    include("includes/header.php");?>
 <div class="image">
 <center><h1 class="text-info"> Hello, <?php echo $_SESSION["name"];?>! </h1>
   <br> <h3>Welcome to Heartbreak Hotel!</h3>
