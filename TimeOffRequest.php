0f00b76a1171e1dc6adfc7cc5e4f88048c42c663
Michael Nutt <michaeltnutt@gmail.com>
2018-11-29 21:34:44 -0600

    Set to default to unchecked

--- TimeOffRequest.php
+++ TimeOffRequest.php
@@ -31,57 +31,75 @@
     <option value="6">Other</option>
   </select>
 </div>
+<?php
+  $selected = date("W");
+?>
 
 <div class="form-group" style = "width:50%;">
 <select class="custom-select">
-<option selected="">Employee</option>
-<option value="1">1</option>
-<option value="2">2</option>
+<option selected="">Week</option>
+<?php
+for($i=1;$i<53;$i++){
+  if($i != $selected) {
+    if($i < 10){
+      echo '<option value="'.$i.'">'.date("m-d",strtotime(date("Y")."W0".$i)).'</option>';
+    } else {
+      echo '<option value="'.$i.'">'.date("m-d",strtotime(date("Y")."W".$i)).'</option>';
+    }
+  } else {
+    echo '<option value="'.$i.'">'.date("m-d",strtotime(date("Y")."W".$i)).' (selected)</option>';
+  }
+}
+?>
 </select>
 </div>
+    <?php
+      echo "<p>Week of ".date("m-d",strtotime("2018W".$selected))."</p>";
+    ?>
 
-      <div class="form-check" style = "margin-left:50px; ">
-        <label class="form-check-label">
-          <input class="form-check-input" type="checkbox" value="" checked="">
-          Sunday
-        </label>
-      </div>
+
 			<div class="form-check" style = "margin-left:50px; ">
 				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" value="" checked="">
+					<input class="form-check-input" name="monday" type="checkbox" value="" >
 					Monday
 				</label>
 			</div>
 			<div class="form-check" style = "margin-left:50px; ">
 				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" value="" checked="">
+					<input class="form-check-input" name="tuesday" type="checkbox" value="" >
 					Tuesday
 				</label>
 			</div>
 			<div class="form-check" style = "margin-left:50px; ">
 				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" value="" checked="">
+					<input class="form-check-input" name="wednesday" type="checkbox" value="" >
 					Wednesday
 				</label>
 			</div>
 			<div class="form-check" style = "margin-left:50px; ">
 				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" value="" checked="">
+					<input class="form-check-input" name="thursday" type="checkbox" value="" >
 					Thursday
 				</label>
 			</div>
 			<div class="form-check" style = "margin-left:50px; ">
 				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" value="" checked="">
+					<input class="form-check-input" name="friday" type="checkbox" value="" >
 					Friday
 				</label>
 			</div>
 			<div class="form-check" style = "margin-left:50px; ">
 				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" value="" checked="">
+					<input class="form-check-input" name="saturday" type="checkbox" value="" >
 					Saturday
 				</label>
 			</div>
+      <div class="form-check" style = "margin-left:50px; ">
+        <label class="form-check-label">
+          <input class="form-check-input" name="sunday" type="checkbox" value="" >
+          Sunday
+        </label>
+      </div>
 <br>
 
   <button type="submit" class="btn btn-primary">Submit Request</button>
@@ -98,10 +116,15 @@
 <h5 style="float: right; margin-right:420px; ">Select the entry to cancel</h5>
 <div class="form-group" style="width:20%; float: right; margin-right:360px;">
    <select class="custom-select">
-     <option selected="">Open this select menu</option>
-     <option value="1">One</option>
-     <option value="2">Two</option>
-     <option value="3">Three</option>
+     <option selected="">Pending Requests</option>
+     <?php
+      $pt = new PTO;
+      $results = $pt->select_one($_SESSION["ID"]);
+      while($row=$results->fetch_assoc())
+      {
+        echo "<option value='".$row["ID"]."'>".date("m-d-y",date("Y")."W".$row["Week"])."</option>";
+      }
+     ?>
    </select>
  </div>
 
