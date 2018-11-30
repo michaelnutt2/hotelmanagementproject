bc4d4382260c03bdf47c65dd1f9214765f8e871a
Miranda Bigby <bigby_miranda@yahoo.com>
2018-11-30 10:17:19 -0600

    Fixed home page, updated the documents folder, updated documents.php for the new document names

--- documents.php
+++ documents.php
@@ -10,20 +10,20 @@
 
     <center>
     <form>
-    <a href="Documents0/eeopost.pdf">
-    <img src="Documents0/EEo.jpg" alt="EEO" style="width:300px;height:400px;">
+    <a href="Documents/eeopost.pdf">
+    <img src="Documents/EEo.jpg" alt="EEO" style="width:300px;height:400px;">
     </a>
 
-    <a href="Documents0/osha3165.pdf">
-    <img src="Documents0/osha.jpg" alt="OSHA" style="width:300px;height:400px;">
+    <a href="Documents/osha3165.pdf">
+    <img src="Documents/osha.jpg" alt="OSHA" style="width:300px;height:400px;">
     </a>
 
-    <a href="Documents0/minwage.pdf">
-    <img src="Documents0/minwage.jpg" alt="MinWage" style="width:300px;height:400px;">
+    <a href="Documents/minwage.pdf">
+    <img src="Documents/minwage.jpg" alt="MinWage" style="width:300px;height:400px;">
     </a>
 
-    <a href="Documents0/fmla.pdf">
-    <img src="Documents0/fmla.jpg" alt="FMLA" style="width:300px;height:400px;">
+    <a href="Documents/fmla.pdf">
+    <img src="Documents/fmla.jpg" alt="FMLA" style="width:300px;height:400px;">
     </a>
 
     <form>
